71ac5ff546fc1343c4f0e23fb32f470578254074
Leigh West <leighwest@hotmail.com>
2024-04-15 17:11:18 +1000

    Added error handling

--- app/lib/meals.tsx
+++ app/lib/meals.tsx
@@ -5,5 +5,6 @@ const db = sql('meals.db');
 
 export async function getMeals(): Promise<MealItemType[]> {
   await new Promise((resolve) => setTimeout(resolve, 5000));
+  // throw new Error();   put here to test that error.tsx works
   return db.prepare('SELECT * FROM meals').all() as MealItemType[];
 }
